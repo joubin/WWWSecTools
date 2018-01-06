93722a3f6f383461145802b26fdf143f71323530
Joubin Jabbari <joubin.j@gmail.com>
2018-01-06 14:28:25 -0800

    threading breaks the end

--- WWWSecTools.py
+++ WWWSecTools.py
@@ -1,11 +1,18 @@
 import csv
-import requests
+import multiprocessing
+import queue
 import socket
 import sys
 import time
-import os
-from concurrent.futures import ThreadPoolExecutor
 from typing import Tuple, Any, List, Union
+from typing import List
+
+from bs4 import BeautifulSoup
+import requests
+import tldextract
+import random
+import string
+import sched, time
 
 from nassl._nassl import OpenSSLError
 from nassl.ssl_client import OpenSslVersionEnum
@@ -14,26 +21,123 @@ from sslyze.utils.ssl_connection import SSLHandshakeRejected
 HTTP = "http://"
 HTTPS = "https://"
 LOG = sys.stdout
+
+ALL_FINISHED = False
+
+
+# import os
 # DEVNULL = open(os.devnull, 'w')
 # LOG = DEVNULL
 
 class CSVWriter:
     def __init__(self, writer, header: List):
         self.writer = writer
+        self.queue = queue.Queue()
         self.write_row(header)
 
+
+    def close(self):
+        global ALL_FINISHED
+        ALL_FINISHED = True
+
     def write_row(self, row: List = None) -> None:
-        if self.writer is not None:
-            self.writer.writerow(row)
-        else:
-            print(row, file=LOG)
+        self.queue.put(row)
+
+    def run(self):
+        global ALL_FINISHED
+        print("Running")
+        while not ALL_FINISHED:
+            print("waiting for something and is finished = %s " % ALL_FINISHED)
+
+            try:
+                row = self.queue.get(timeout=3)
+
+            except queue.Empty:
+                pass
+            finally:
+                if row is not None:
+                    if self.writer is not None:
+                        self.writer.writerow(row)
+                    else:
+                        print(row, file=LOG)
+        print("CSV Writer Done")
+
+
+class ParkedDomain:
+    PARK_SERVICE = ["smartname.com", "sedo.com", "parkingcrew.com",
+                    "uniregistry.com", "hugedomains.com"]
+
+    def __init__(self, url, schema=HTTP):
+        self.url = url
+        self.schema = schema
+        self.soup = self.__get_page()
+
+    def __get_page(self):
+        try:
+            response = requests.get(url=self.schema + self.url,
+                                    headers=Domain.HEADERS)
+            content = response.content
+            return BeautifulSoup(content, 'html.parser')
+        except (requests.exceptions.ConnectionError):
+            return None
+
+    def has_parking_service_resources(self) -> bool:
+        resources = [self.find_list_resources("script", "src"),
+                     self.find_list_resources("img", "src")]
+        clean = []
+        for resource in resources:
+            for each_item in resource:
+                clean.append(ParkedDomain.get_domain(each_item))
+        something = list(set(clean) & set(ParkedDomain.PARK_SERVICE))
+        return len(something) > 0
+
+    def domain_has_random_subdomains(self) -> bool:
+        subdomain = ''.join(
+            random.SystemRandom().choice(string.ascii_uppercase + string.digits)
+            for _ in range(10))
+        url = HTTP + subdomain + '.' + self.url
+
+        try:
+            response = requests.get(url=url, headers=Domain.HEADERS, )
+        except requests.exceptions.ConnectionError:
+            return False
+        if 'Location' in response.headers:
+            if str(response.headers['Location']) == '':
+                return True
+        return False
+
+    def is_parked(self):
+        return self.has_parking_service_resources() or \
+               self.domain_has_random_subdomains()
+
+    @staticmethod
+    def get_domain(url: str) -> str:
+        extracted = tldextract.extract(url)
+        return "{}.{}".format(extracted.domain, extracted.suffix)
+
+    def find_list_resources(self, tag, attribute) -> List:
+        list = []
+        if self.soup is None:
+            return []
+
+        for x in self.soup.findAll(tag):
+            try:
+                list.append(x[attribute])
+            except KeyError:
+                pass
+        return list
 
 
 class Domain:
-    csv_format = ['URL', 'HTTP', 'HTTPS', 'HSTS', 'HTTPS Redirect', 'SSLV23',
+    csv_format = ['URL', 'HTTP', 'HTTPS', 'Parked', 'HSTS', 'HTTPS Redirect',
+                  'SSLV23',
                   'SSLV2', 'SSLV3', 'TLSV1', 'TLSV1_1', 'TLSV1_2', 'TLSV1_3']
 
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/63.0.3239.84 Safari/537.36'}
+
     def __init__(self, url: str, csv_writer: CSVWriter):
         self.url: str = Domain.remove_schema(url)
         self.has_http: bool = False
@@ -42,6 +146,7 @@ class Domain:
         self.https_response: requests.Response = None
         self.hsts = False
         self.https_redirect = False
+        self.is_parked = False
         self.ssl_version_support = {}
 
         for item in OpenSslVersionEnum:
@@ -58,12 +163,12 @@ class Domain:
             print("\tTesting %s ---> can connect http" % (self.url,), file=LOG)
             can_connect = self.can_connect(schema=HTTP)
             if can_connect[0]:
-
                 self.has_http = True
                 self.http_response = can_connect[1]
                 print("\tTesting %s ---> redirect" % (self.url,), file=LOG)
                 self.https_redirect = self.is_https_redirect(
                     response=self.http_response)
+                self.is_parked = self.is_domain_parked(schema=HTTP)
         print("\tTesting %s ---> Port 443" % (self.url,), file=LOG)
         port443 = self.has_open_port(port=443)
         if port443:
@@ -75,20 +180,16 @@ class Domain:
                 self.hsts = self.has_hsts()
                 print("\tTesting %s ---> crypto" % (self.url,), file=LOG)
                 self.crypt_stuff()
+                self.is_parked = self.is_domain_parked(schema=HTTPS)
             # check ssl versions
             # check tls version
             # check other tls stuff
-        print("Finished: %s" % (self.url))
         self.write_to_csv()
+        print("Finished: %s" % self.url, file=LOG)
 
-    def is_domain_parked(self) -> bool:
-        raise NotImplementedError
-        # pull the page and check for *park*.css and *park*.js
-        # pull / and /+random data, if pages match, its probably parked
-        # each visit to / resutls in a different url being tacked on
-        # redirects to a site one of these sites
-        ["https://uniregistry.com"]
-
+    def is_domain_parked(self, schema=HTTP) -> bool:
+        pd = ParkedDomain(url=self.url, schema=schema)
+        return pd.is_parked()
 
     def crypt_stuff(self):
         import sslyze.server_connectivity as sc
@@ -107,7 +208,8 @@ class Domain:
         Connect to target site and check its headers."
         """
         try:
-            self.https_response = requests.get(HTTPS + self.url, headers=Domain.HEADERS)
+            self.https_response = requests.get(HTTPS + self.url,
+                                               headers=Domain.HEADERS)
         except requests.exceptions.SSLError as error:
             print("An error for %s when checking HSTS. \nError: %s" % (
                 self.url, error,),
@@ -121,7 +223,10 @@ class Domain:
 
     def has_open_port(self, port: int = 80) -> bool:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((self.url, port))
+        try:
+            result = sock.connect_ex((self.url, port))
+        except socket.gaierror:
+            return False
         # noinspection PyBroadException
         try:
             sock.close()
@@ -152,13 +257,17 @@ class Domain:
         code301 = response.status_code == 301
         return code301 == locationHTTPS
 
-    def can_connect(self, schema: str = HTTP, timeout:int = 10) -> Tuple[bool, Any]:
+    def can_connect(self, schema: str = HTTP, timeout: int = 10) -> Tuple[
+        bool, Any]:
         """
         :return: Tuple. Index 0 is the http and Index 1 is https
         """
         try:
-            httpResponse = requests.get(schema + self.url, timeout=timeout, headers=Domain.HEADERS)
-        except requests.exceptions.ConnectionError:
+            httpResponse = requests.get(schema + self.url, timeout=timeout,
+                                        headers=Domain.HEADERS)
+        except (
+                requests.exceptions.ConnectionError,
+                requests.exceptions.ReadTimeout):
             return False, None
 
         return True, httpResponse
@@ -169,7 +278,8 @@ class Domain:
         """
 
         def __make_data() -> List[Union[str, bool]]:
-            columns = [self.url, self.has_http, self.has_https, self.hsts,
+            columns = [self.url, self.has_http, self.has_https, self.is_parked,
+                       self.hsts,
                        self.https_redirect]
             for i in OpenSslVersionEnum:
                 columns.append(self.ssl_version_support[i])
@@ -205,21 +315,36 @@ def run():
               sys.argv[0] + " inputFile.txt outputFile.csv", file=sys.stderr,
               flush=True)
         sys.exit(31)
-
+    jobs: List[multiprocessing.Process] = []
+    domains: List[Domain] = []
+    csv_writer_process = None
+    csv_writer = None
     start = time.time()
+
     with open(inputFile, "r") as in_file:
         with open(outputFile, 'w') as out_file:
-            with ThreadPoolExecutor(max_workers=10) as executor:
-                csv_writer = CSVWriter(
-                    writer=csv.writer(out_file, delimiter=','),
-                    header=Domain.csv_format)
-                for line in in_file:
-                    url = line
-                    domain = Domain(url=url, csv_writer=csv_writer)
-                    executor.submit(fn=domain.run)
+            csv_writer = CSVWriter(
+                writer=csv.writer(out_file, delimiter=','),
+                header=Domain.csv_format)
+            csv_writer_process = multiprocessing.Process(target=csv_writer.run,
+                                                         name="writer")
+            csv_writer_process.start()
+            for line in in_file:
+                url = line
+                domain = Domain(url=url, csv_writer=csv_writer)
+                domains.append(domain)
+                job = multiprocessing.Process(target=domain.run, name=url)
+                jobs.append(job)
+                job.start()
 
+    for job in jobs:
+        print("waiting on %s" % job.name)
+        job.join()
+    print("All done")
+    csv_writer.close()
+    csv_writer_process.join()
     end = time.time()
-    print("took %s seconds to run" %(end-start,))
+    print("took %s seconds to run" % (end - start,))
 
 
 def test():
@@ -231,6 +356,29 @@ def test():
     from ParkDetection import ParkedDomain as PD
     p = PD()
 
+
+def test_random_domains():
+    from itertools import product
+    from string import ascii_lowercase
+    keywords = [''.join(i) for i in product(ascii_lowercase, repeat=5)]
+    jobs: List[multiprocessing.Process] = []
+    with open('output.csv', 'w') as out_file:
+        csv_writer = CSVWriter(
+            writer=csv.writer(None, delimiter=','),
+            header=Domain.csv_format)
+        for schema in ['.com', '.net']:
+            for domain in keywords:
+                url = domain + schema
+                print(url)
+                park = Domain(url=url, csv_writer=csv_writer)
+                process = multiprocessing.Process(target=park.run)
+                jobs.append(process)
+                process.start()
+        for i in jobs:
+            i.join()
+
+
 if __name__ == '__main__':
-    # run()
-    test()
+    run()
+    # test()
+    # test_random_domains()
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 # WWWSecTools
+## test
