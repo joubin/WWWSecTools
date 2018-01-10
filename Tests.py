e96f58d37acde2067a9925a48fcd431131ed4555
Joubin Jabbari <joubin.j@gmail.com>
2018-01-09 19:55:06 -0800

    removed files that should not be part of git

--- Tests.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    from WWWSecTools import *
-    dataset1 = ["pcyr.org", "payd.org", "frbchi.com", "jabbari.io", "godaddy.com", "frbny.com", "securepaymenttaskforce.com"]
-    dataset2 = ["diply.com"]
-    for i in dataset1:
-        pd = ParkedDomain(url=i, schema=HTTP)
-        print("is %s parked: %s" % (i,pd.is_parked()))
